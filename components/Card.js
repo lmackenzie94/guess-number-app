42be61572657f4dadb9b3417989071e1efbf31fe
Luke MacKenzie <luke.mackenzie@campjefferson.com>
2019-12-09 09:22:24 -0500

    first commit

---
+++ components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Card = ({ children, style }) => {
+  return <View style={{ ...styles.card, ...style }}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    // elevation is andriod specific as 'shadow' properties only work with iOS
+    elevation: 8,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10
+  }
+});
+export default Card;
